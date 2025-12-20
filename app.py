dc0139db322ee1d20e13a30d7ea80775ae4fee93
saioao <saioa.otegim@gmail.com>
2025-12-20 01:40:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ h1, h2, h3, h4, h5, h6, p, span, div {
     color: #ffffff;             /* testua zuria */
     font-weight: bold;
     border-radius: 4px;
+    border-color: black
 }
 </style>
 """, unsafe_allow_html=True)
