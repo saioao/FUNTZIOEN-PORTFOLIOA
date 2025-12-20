d9235c853f94bcf605dbc9847da2f857a4b01d71
saioao <saioa.otegim@gmail.com>
2025-12-20 01:55:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,9 @@ h1, h2, h3, h4, h5, h6, p, span, div {
     display: inline-block;
     margin-bottom: 6px;   
 }
+<button onclick="window.location.reload();" style="
+    font-weight: bold;
+}
 </style>
 """, unsafe_allow_html=True)
 
