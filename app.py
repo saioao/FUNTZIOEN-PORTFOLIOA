933e780289a6977a07b986415039b9acb909e212
saioao <saioa.otegim@gmail.com>
2026-01-02 14:12:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ with col_center:
 
     try:
         f = sp.sympify(f_clean, locals={"e": sp.E, "pi": sp.pi})
-        x_vals = np.linspace(-10, 10, 2000)
+        x_vals = np.linspace(-20, 20, 2000)
 
         if f.free_symbols == set():
             y_vals = np.full_like(x_vals, float(f))
